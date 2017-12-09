30ef7f1f68c29fca393a456d8a4c6fdd4a7ad451
mtotodev05 <mtotodev05@gmail.com>
2017-12-09 21:45:20 +0300

    Passed Tests for Mysql

--- src/main/java/com/brian/Model/Base.java
+++ src/main/java/com/brian/Model/Base.java
@@ -5,9 +5,10 @@ import java.sql.*;
 //Defines the database Connection and Schemas for Mysql
 public abstract class Base implements Utils {
 
+    public static String driver = "com.mysql.cj.Driver";
     public static String username = "root";
     public static String password = "wamatu";
-    public static String url ="jdbc:mysql://localhost:3306";
+    public static String url ="jdbc:mysql://localhost:3306/TelegramBot?useSSL=false";
     protected Connection connection = null;
     protected Statement statement = null;
     protected ResultSet resultSet = null;
@@ -15,6 +16,16 @@ public abstract class Base implements Utils {
 
     @Override
     public boolean getConnection() throws SQLException{
+        try{
+
+            connection=DriverManager.getConnection(url,username,password);
+
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }finally {
+            closeConnection();
+        }
+
         return true;
     }
 
---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,23 @@
+import com.brian.Model.Base;
+import org.junit.Test;
+
+import java.sql.SQLException;
+
+import static org.junit.Assert.assertEquals;
+
+public class BaseTest {
+
+
+    @Test
+    public void testDatabaseConnection() throws SQLException{
+        Base instance = new BaseImpl();
+        boolean expectedResult = true;
+        boolean result = instance.getConnection();
+        assertEquals(expectedResult,result);
+    }
+
+
+    public class BaseImpl extends Base{
+
+    }
+}
