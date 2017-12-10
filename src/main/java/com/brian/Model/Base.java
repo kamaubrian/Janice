b11eb35b605b1f4dddef2d91aed84809868d9bea
mtotodev05 <mtotodev05@gmail.com>
2017-12-10 15:28:09 +0300

    Added Tables

--- src/main/java/com/brian/Model/Base.java
+++ src/main/java/com/brian/Model/Base.java
@@ -14,6 +14,24 @@ public abstract class Base implements Utils {
     protected ResultSet resultSet = null;
     protected PreparedStatement preparedStatement = null;
 
+    protected Base(){
+        String sql;
+        try{
+            getConnection();
+            statement = connection.createStatement();
+            sql="CREATE TABLE IF NOT EXISTS User(" +
+                    "ID INT NOT NULL AUTO_INCREMENT," +
+                    "Chat_ID INT NOT NULL," +
+                    "Username VARCHAR(35) NOT NULL," +
+                    "Created_At TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP," +
+                    "PRIMARY KEY(ID))";
+            statement.addBatch(sql);
+            statement.executeBatch();
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
     @Override
     public boolean getConnection() throws SQLException{
         try{
@@ -22,16 +40,13 @@ public abstract class Base implements Utils {
 
         }catch(Exception ex){
             ex.printStackTrace();
-        }finally {
-            closeConnection();
         }
-
         return true;
     }
 
     @Override
     public boolean closeConnection() throws SQLException{
-        if(getConnection()){
+        if(!connection.isClosed()){
             connection.close();
         }
         return true;
