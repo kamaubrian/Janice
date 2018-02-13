daf3b3c304c22126807beaae6afd9d775f67f715
mtotodev05 <mtotodev05@gmail.com>
2018-02-13 21:06:47 +0300

    Cleared bugs

--- src/main/java/com/brian/Controller/MessageBot/Main.java
+++ src/main/java/com/brian/Controller/MessageBot/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 /*
    Author: Brian Kamau
  */
+import com.brian.Controller.PhotoBot.PhotoBot;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
@@ -23,9 +24,25 @@ public class Main {
                 Thread.sleep(200);
             }
             System.out.println("Telegram Bots by: [Brian Kamau]");
-            System.out.println("Choose Bot");
+            System.out.println("Choose Bot\n1.Message Bot\n2.Photo Bot");
+            choice = scanner.nextInt();
+            switch(choice){
 
-            botApi.registerBot(new Bot());
+                case 1:
+                    System.out.println("Messaging Bot is Running");
+                    botApi.registerBot(new Bot());
+                    break;
+
+                case 2:
+                    System.out.println("Photo Bot is Running");
+                    botApi.registerBot(new PhotoBot());
+                    break;
+
+                default:
+                    System.out.println("Invalid Choice");
+                    break;
+
+            }
         }catch(TelegramApiException e){
             e.printStackTrace();
         } catch (InterruptedException e) {
