4ddb62bc930799aa84d3dd7ea223adb93dde489d
mtotodev05 <mtotodev05@gmail.com>
2018-02-13 20:53:33 +0300

    Updated Janice

--- src/main/java/com/brian/Controller/MessageBot/Main.java
+++ src/main/java/com/brian/Controller/MessageBot/Main.java
@@ -1,5 +1,5 @@
 package com.brian.Controller.MessageBot;
-
+import java.util.Scanner;
 /*
    Author: Brian Kamau
  */
@@ -8,6 +8,8 @@ import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
+    static int choice;
 
     public static void main(String [] args){
         ApiContextInitializer.init();
@@ -15,9 +17,21 @@ public class Main {
         TelegramBotsApi botApi = new TelegramBotsApi();
 
         try{
+            System.out.println("Bot is Running");
+            for(int i=0;i<5;i++){
+                System.out.println(".");
+                Thread.sleep(200);
+            }
+            System.out.println("Telegram Bots by: [Brian Kamau]");
+            System.out.println("Choose Bot");
+
             botApi.registerBot(new Bot());
         }catch(TelegramApiException e){
+            e.printStackTrace();
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 }
+
+
