a247aac7a272205d49dee2cb310b4b0ccddb42cb
mtotodev05 <mtotodev05@gmail.com>
2018-02-25 23:45:34 +0300

    Implemented Dictionary

--- src/main/java/com/brian/Controller/MessageBot/Bot.java
+++ src/main/java/com/brian/Controller/MessageBot/Bot.java
@@ -2,11 +2,18 @@ package com.brian.Controller.MessageBot;
 /*
   Author:  Brian Kamau
  */
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 import com.brian.Model.BotCredentials;
+import org.json.simple.parser.JSONParser;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -16,6 +23,7 @@ public class Bot  extends TelegramLongPollingBot{
 
     public void onUpdateReceived(Update update) {
         String new_message="";
+        JSONParser parser = new JSONParser();
         long chat = update.getMessage().getChatId();
         String message_case="";
         String user_first = update.getMessage().getChat().getFirstName();
@@ -23,8 +31,25 @@ public class Bot  extends TelegramLongPollingBot{
         if(update.hasMessage() && update.getMessage().hasText()) {
 
            message_case = update.getMessage().getText();
+           try{
+               Object dictionary = parser.parse(new FileReader("dictionary.json"));
+               JSONObject jsonObject = (JSONObject) dictionary;
+               JSONArray answer = (JSONArray) jsonObject.get(message_case.toLowerCase());
+               if(!answer.isEmpty()){
+                   new_message = answer.toString();
+               }else{
+                   new_message = "Word Not Found";
+               }
 
-       }
+           }catch(FileNotFoundException ex){
+               ex.printStackTrace();
+           } catch (ParseException e) {
+               e.printStackTrace();
+           } catch (IOException e) {
+               e.printStackTrace();
+           }
+
+        }
 
             SendMessage message = new SendMessage()
                     .setChatId(chat)
